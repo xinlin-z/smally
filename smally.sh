5894e2d3d09a72f4a700e77f3a89d8712968cc66
xinlin <xinlin.zhang@hotmail.com>
2022-06-17 14:35:32 +0800

    refactor smally.sh and test.sh

--- smally.sh
+++ smally.sh
@@ -3,23 +3,21 @@ set -u
 
 
 dn=$(dirname $0)
+filetype=$(file $1 | awk '{print $2}')
 
-
-file $1 | grep 'JPEG' > /dev/null
-if [ $? -eq 0 ]; then
+if [ $filetype == 'JPEG' ]; then
     python3 ${dn}/smally.py --jpegtran $1
     exit
 fi
 
-
-file $1 | grep 'PNG' > /dev/null
-if [ $? -eq 0 ]; then
+if [ $filetype == 'PNG' ]; then
     python3 ${dn}/smally.py --optipng $1
     exit
 fi
 
-
-file $1 | grep 'GIF' > /dev/null
-if [ $? -eq 0 ]; then
+    
+if [ $filetype == 'GIF' ]; then
     python3 ${dn}/smally.py --gifsicle $1
+    exit
 fi
+
--- test.sh
+++ test.sh
@@ -1,8 +1,20 @@
 # need to run in smally repos folder
-set -eux
+set -eu
 
+
+echo 'test python interface...'
 cp -r testpic _ttpic
 python3 smally.py --jpegtran _ttpic/102.jpg
 python3 smally.py --optipng _ttpic/201.png
 python3 smally.py --gifsicle _ttpic/302.gif
 rm -rf _ttpic 
+
+
+echo 'test bash interface...'
+cp -r testpic _ttpic
+find _ttpic -type f -exec bash smally.sh {} \;
+rm -rf _ttpic 
+
+
+echo 'Test Done!'
+

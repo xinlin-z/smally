81c3dac33e811c72d3cce39216c1bf5d93fe026d
xinlin <xinlin.zhang@hotmail.com>
2022-07-23 17:55:06 +0800

    refactor

--- smally.sh
+++ smally.sh
@@ -5,19 +5,9 @@ set -u
 dn=$(dirname $0)
 filetype=$(file $1 | awk '{print $2}')
 
-if [ $filetype == 'JPEG' ]; then
-    python3 ${dn}/smally.py --jpegtran $1
-    exit
-fi
-
-if [ $filetype == 'PNG' ]; then
-    python3 ${dn}/smally.py --optipng $1
-    exit
-fi
-
-    
-if [ $filetype == 'GIF' ]; then
-    python3 ${dn}/smally.py --gifsicle $1
-    exit
-fi
+case $filetype in
+    JPEG) python3 ${dn}/smally.py --jpegtran $1;;
+    PNG)  python3 ${dn}/smally.py --optipng $1;;
+    GIF)  python3 ${dn}/smally.py --gifsicle $1;;
+esac
 

42d2911779ebba0f13d70f086123c9eb4a8e5635
xinlin-z <xinlin.zhang@hotmail.com>
2023-06-01 16:07:30 +0800

    tweaks

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 set -u
 
 
-echo '# test smally.py ...'
+echo '# Test smally.py'
 cp -r testpic _ttpic
 python3 smally.py --jpegtran _ttpic/102.jpg
 python3 smally.py --optipng _ttpic/201.png
@@ -10,7 +10,7 @@ python3 smally.py --gifsicle _ttpic/302.gif
 rm -rf _ttpic
 
 
-echo '# test smally.sh ...'
+echo '# Test smally.sh'
 cp -r testpic _ttpic
 bash smally.sh _ttpic/102.jpg
 bash smally.sh _ttpic/201.png
@@ -18,33 +18,38 @@ bash smally.sh _ttpic/302.gif
 rm -rf _ttpic
 
 
-echo '# test smally.sh -t ...'
+function run_with_wrong_type(){
+    bash smally.sh -t ABCD $1 > /dev/null
+    if [ $? -ne 1 ]; then
+        echo 'exit code is not expected'
+        rm -rf _ttpic
+        exit 1
+    fi
+}
+
+
+echo '# Test smally.sh -t'
 cp -r testpic _ttpic
 bash smally.sh -t JPEG _ttpic/102.jpg
 bash smally.sh -t PNG _ttpic/201.png
 bash smally.sh -t GIF _ttpic/302.gif
-bash smally.sh -t KKK _ttpic/302.gif
-if [ $? -ne 1 ]; then
-    echo 'exit code is not expected'
-    exit 1
-else
-    echo '-t KKK test ok'
-fi
+run_with_wrong_type _ttpic/102.jpg
+run_with_wrong_type _ttpic/201.png
+run_with_wrong_type _ttpic/302.gif
 rm -rf _ttpic
 
 
-echo '# test find with smally.sh ...'
+echo '# Test find with smally.sh'
 cp -r testpic _ttpic
 find _ttpic -type f -exec bash smally.sh {} \;
 rm -rf _ttpic
 
 
-echo '# test find with smally.sh -t...'
+echo '# Test find with smally.sh -t'
 cp -r testpic _ttpic
 find _ttpic -type f -exec bash smally.sh -t JPEG {} \;
 find _ttpic -type f -exec bash smally.sh -t PNG {} \;
 find _ttpic -type f -exec bash smally.sh -t GIF {} \;
-find _ttpic -type f -exec bash smally.sh -t KKK {} \;
 rm -rf _ttpic
 
 
